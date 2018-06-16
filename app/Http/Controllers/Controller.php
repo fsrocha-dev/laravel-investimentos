d5fe703f1d251fc2f1ea4a4ec21c7fcdffeb9e79
fsrocha-dev <fsrocha.dev@gmail.com>
2018-06-16 11:02:30 -0300

    Refactoring routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,12 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function home()
+    {
+    	$titulo = "Laravel Investimentos";
+    	return view('welcome', [
+    		'title' => $titulo
+    	]);
+    }
 }
